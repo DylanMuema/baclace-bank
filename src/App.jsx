09e36fccb4e130e7fbe6e88ee38a24a49ab69339
DYLANMUEMA <dylanmuemamunyao@gmail.com>
2025-06-02 17:10:37 +0000

    commit change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/hero"; 
 import HowItWorks from "./components/Howitworks";
 import Services from "./components/Services";
